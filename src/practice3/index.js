936b49bb3c7dedfb17a57e35e23026c1e378a5cd
wmxl <294036748@qq.com>
2020-11-13 14:01:16 +0800

    [yiming] add solution2 to practice3!

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,7 +1,18 @@
+// soulution1
 export const getPoetry = async () => {
   const response = await fetch("https://v1.jinrishici.com/all.json", {
     method: "GET",
   });
+  // a
   const data = await response.json();
   return [data.origin, data.author, data.content];
 };
+
+// solution 2
+// export const getPoetry = async () => {
+//   return fetch("https://v1.jinrishici.com/all.json", {
+//     method: "GET",
+//   })
+//       .then((res) => res.json())
+//       .then((data) => [data.origin, data.author, data.content]);
+// };
