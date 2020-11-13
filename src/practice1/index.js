1a82648bf877deea0185ca991745231aad284898
wmxl <294036748@qq.com>
2020-11-13 13:22:00 +0800

    [yiming] finish 3 practices

--- src/practice1/index.js
+++ src/practice1/index.js
@@ -1,1 +1,7 @@
+import { REGION, ADDRESS, PORT, PATH } from "./constant";
+import getURL from "./utils";
 
+const region = REGION;
+const url = getURL(ADDRESS, PORT, PATH);
+
+export { url, region };
--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,7 @@
-export const getPoetry
+export const getPoetry = async () => {
+  const response = await fetch("https://v1.jinrishici.com/all.json", {
+    method: "GET",
+  });
+  const data = await response.json();
+  return [data.origin, data.author, data.content];
+};
